b831d60b87c844dd362b2da495bfa84298f7f528
JFBizueque <71117126+JFBizueque@users.noreply.github.com>
2026-05-16 19:24:17 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_HOST', 'database-5020475061.webspace-host.com');
+define('DB_NAME', 'dbs15682559');
+define('DB_USER', 'dbu271506');
+define('DB_PASS', '4ed12f81e1758806910');
+
+?>

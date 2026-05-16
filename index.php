4be2341e692f9b25b659896cf54b4b3dd30c21e4
JFBizueque <71117126+JFBizueque@users.noreply.github.com>
2026-05-16 19:10:26 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Login System</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="container">
+
+    <h1>Willkommen</h1>
+
+    <a href="register.php">
+        <button>Registrieren</button>
+    </a>
+
+    <a href="login.php">
+        <button>Login</button>
+    </a>
+
+</div>
+
+</body>
+</html>
